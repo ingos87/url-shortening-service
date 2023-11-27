9a55eb0ea85ab75b13fa45027d6629b68203c230
Ingo Schwarz <ingo.schwarz@dpdhl.com>
2023-11-27 21:06:49 +0100

    init empty gradle kotlin spring boot project

---
+++ src/main/kotlin/codingchallenge/urlshorteningservice/UrlShorteningServiceApplication.kt
@@ -0,0 +1,11 @@
+package codingchallenge.urlshorteningservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlShorteningServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<UrlShorteningServiceApplication>(*args)
+}
